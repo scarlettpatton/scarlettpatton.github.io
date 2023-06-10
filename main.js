1e08301016390e091d2825ab518a7a9565317d27
Scarlett <scarlettdpatton@gmail.com>
2023-06-10 14:33:37 -0500

    add css and js

---
+++ main.js
@@ -0,0 +1,15 @@
+function toAbout() {
+    window.location.href = "about.html";
+}
+function toResume() {
+    window.location.href = "resume.html";
+}
+function toProjects() {
+    window.location.href = "projects.html";
+}
+function toContact() {
+    window.location.href = "contact.html";
+}
+function toHome() {
+    window.location.href = "index.html";
+}
